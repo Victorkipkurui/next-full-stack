b14ebb467539769de86d6730966c81cd17eead30
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-10-27 22:24:21 +0300

    next

---
+++ app/button/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div className='p-10  text-center'>
+      <Button className=' bg-blue-700'>Click me</Button>
+    </div>
+  )
+}
